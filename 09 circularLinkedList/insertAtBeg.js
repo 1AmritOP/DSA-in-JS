1fc1e9e24065db43e89ae76180b844f24e8add11
Amrit Kumar <amrit24680gg@gmail.com>
2025-09-02 11:45:49 +0530

    write constant time solution

--- 09 circularLinkedList/insertAtBeg.js
+++ 09 circularLinkedList/insertAtBeg.js
@@ -1,6 +1,8 @@
 // insert a node at the beginning of a circular linked list
 // ex-> 1->2->3->4->5->1 => 6 at beginning => 6->1->2->3->4->5->6
 
+
+//1st approach
 function insertAtBeg(head,data){
     let newNode=new Node(data);
     if(!head){
@@ -14,4 +16,19 @@ function insertAtBeg(head,data){
     curr.next=newNode;
     newNode.next=head;
     return newNode;
+}
+
+//2nd approach (constant time)
+
+function insertAtBeg(head,data){
+    let newNode= new Node(data);
+    if (!head) {
+        newNode.next=newNode;
+        return newNode;
+    }
+
+    newNode.next=head.next;
+    head.next=newNode;
+    [head.data,newNode.data]=[newNode.data,head.data];
+    return head;
 }
