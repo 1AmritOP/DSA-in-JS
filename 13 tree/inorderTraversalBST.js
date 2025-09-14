0e1d73996ed899b73ce0d178118a6bbe0e4c2c9a
Amrit Kumar <amrit24680gg@gmail.com>
2025-09-14 18:59:27 +0530

    implementing inorder traversal in BST

---
+++ 13 tree/inorderTraversalBST.js
@@ -0,0 +1,26 @@
+// implementing inorder traversal in BST
+
+class Node{
+    constructor(key){
+        this.key=key;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+let root=new Node(5);
+root.left=new Node(3);
+root.right=new Node(6);
+root.left.left=new Node(2);
+root.left.right=new Node(4);
+root.right.right=new Node(7);
+
+function inorder(root){
+    if (root !== null) {
+        inorder(root.left);
+        console.log(root.key);
+        inorder(root.right);
+    }
+}
+
+inorder(root);
