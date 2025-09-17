e095fbe6f9da5d7804fcbad7ce521f12cb498ca5
Amrit Kumar <amrit24680gg@gmail.com>
2025-09-17 23:49:35 +0530

    Level Order Traversal - Line by Line

--- 13 tree/levelOrderTraversal.js
+++ 13 tree/levelOrderTraversal.js
@@ -12,3 +12,23 @@ function levelOrderTraversal(root){
         if(curr.right !== null) q.enque(curr.right);
     }
 }
+
+
+// Level Order Traversal - Line by Line
+
+function levelOrderTraversal(root){
+    if (!root) return;
+    let q= new Queue();
+    q.enque(root);
+    q.enque(null);
+    while(!q.isEmpty()){
+        let res=q.deque();
+        if (res) {
+            console.log(res.data);
+            if(res.left !== null) q.enque(res.left);
+            if(res.right !== null) q.enque(res.right);
+        }else{
+            console.log("\n");
+        }
+    }
+}
