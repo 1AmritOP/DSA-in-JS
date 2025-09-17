ad7cf6b02d4450c23c10011a2a710fe560c1a3d8
Amrit Kumar <amrit24680gg@gmail.com>
2025-09-18 00:01:44 +0530

    corrected the Function

--- 13 tree/levelOrderTraversal.js
+++ 13 tree/levelOrderTraversal.js
@@ -1,34 +1,37 @@
 // level order traversal of a binary tree
 
-function levelOrderTraversal(root){
-    if (!root) return;
-    let q= new Queue();
-    q.enque(root);
+function levelOrderTraversal(root) {
+  if (!root) return;
+  let q = new Queue();
+  q.enque(root);
 
-    while (!q.isEmpty()) {
-        let curr=q.deque();
-        console.log(curr);
-        if(curr.left !== null) q.enque(curr.left);
-        if(curr.right !== null) q.enque(curr.right);
-    }
+  while (!q.isEmpty()) {
+    let curr = q.deque();
+    console.log(curr.key);
+    if (curr.left !== null) q.enque(curr.left);
+    if (curr.right !== null) q.enque(curr.right);
+  }
 }
-
 
 // Level Order Traversal - Line by Line
 
-function levelOrderTraversal(root){
-    if (!root) return;
-    let q= new Queue();
-    q.enque(root);
-    q.enque(null);
-    while(!q.isEmpty()){
-        let res=q.deque();
-        if (res) {
-            console.log(res.data);
-            if(res.left !== null) q.enque(res.left);
-            if(res.right !== null) q.enque(res.right);
-        }else{
-            console.log("\n");
-        }
+function levelOrderTraversal(root) {
+  let ans = "";
+  if (!root) return;
+  let q = new Queue();
+  q.enque(root);
+  q.enque(null);
+  while (q.size() > 1) {
+    let curr = q.deque();
+    if (curr === null) {
+      ans += "\n";
+      q.enque(null);
+      continue;
     }
+      ans += curr.key + " ";
+      if (curr.left !== null) q.enque(curr.left);
+      if (curr.right !== null) q.enque(curr.right);
+  }
+
+  return ans;
 }
