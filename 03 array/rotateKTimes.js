8554eb44eda44fd54513921e17c76f274f405c21
Amrit Kumar <amrit24680gg@gmail.com>
2025-09-30 00:00:56 +0530

    rotate an array by k times to the right

---
+++ 03 array/rotateKTimes.js
@@ -0,0 +1,38 @@
+// rotate an array by k times to the right
+// ex-> [1,2,3,4,5] => k=2 => [4,5,1,2,3]
+// ex-> [1,2,3,4,5,6,7] => k=3 => [5,6,7,1,2,3,4]
+
+// using extra space
+function rotateArrayByKTimes(arr, k) {
+    const n = arr.length;
+    const result = new Array(n);
+
+    for (let i = 0; i < n; i++) {
+        result[(i + k) % n] = arr[i];
+    }
+
+    return result;
+}
+
+
+// without using extra space
+function rotateKTimes(arr, k) {
+    n = arr.length
+    k = k % n;
+    reverseArray(arr, 0, n-k - 1);
+    reverseArray(arr, n-k, n - 1);
+    reverseArray(arr, 0, n - 1);
+}
+
+function reverseArray(arr, start, end) {
+    while (start < end) {
+        [arr[start], arr[end]] = [arr[end], arr[start]];
+        start++;
+        end--;
+    }
+}
+
+
+let arr=[1,2,3,4,5,6,7];
+rotateKTimes(arr,3);
+console.log(arr);
